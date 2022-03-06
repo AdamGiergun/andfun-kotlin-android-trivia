9f97097bc63cf54707a32f7bf88673da5277bf5c
AdamGiergun <adasss@poczta.onet.pl>
2022-03-06 14:55:30 +0100

    Remove warning about getShareIntent().resolveActivity by adding <queries> to AndroidManifest.xml

--- app/src/main/java/com/example/android/navigation/GameWonFragment.kt
+++ app/src/main/java/com/example/android/navigation/GameWonFragment.kt
@@ -19,10 +19,8 @@ package com.example.android.navigation
 import android.content.Intent
 import android.os.Bundle
 import android.view.*
-import android.widget.Toast
 import androidx.core.app.ShareCompat
 import androidx.fragment.app.Fragment
-import androidx.navigation.Navigation
 import androidx.navigation.findNavController
 import com.example.android.navigation.databinding.FragmentGameWonBinding
 
