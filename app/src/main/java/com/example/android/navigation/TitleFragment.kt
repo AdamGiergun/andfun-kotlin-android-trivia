09d59ebe0c8d76420befb9ea466cdb12a7bbd84b
AdamGiergun <adasss@poczta.onet.pl>
2022-03-06 14:56:02 +0100

    Remove unused import directives

--- app/src/main/java/com/example/android/navigation/TitleFragment.kt
+++ app/src/main/java/com/example/android/navigation/TitleFragment.kt
@@ -3,7 +3,6 @@ package com.example.android.navigation
 import android.os.Bundle
 import android.view.*
 import androidx.fragment.app.Fragment
-import androidx.navigation.Navigation
 import androidx.navigation.findNavController
 import androidx.navigation.ui.NavigationUI
 import com.example.android.navigation.databinding.FragmentTitleBinding
