500e814cb644e49860c4182e821323561bdff8d9
AdamGiergun <adasss@poczta.onet.pl>
2022-03-06 15:34:29 +0100

    Rewrite code

--- app/src/main/java/com/example/android/navigation/MainActivity.kt
+++ app/src/main/java/com/example/android/navigation/MainActivity.kt
@@ -18,7 +18,6 @@ package com.example.android.navigation
 
 import android.os.Bundle
 import androidx.appcompat.app.AppCompatActivity
-import androidx.databinding.DataBindingUtil
 import androidx.drawerlayout.widget.DrawerLayout
 import androidx.navigation.NavController
 import androidx.navigation.NavDestination
@@ -35,25 +34,33 @@ class MainActivity : AppCompatActivity() {
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        @Suppress("UNUSED_VARIABLE")
-        val binding = DataBindingUtil.setContentView<ActivityMainBinding>(this, R.layout.activity_main)
-        drawerLayout = binding.drawerLayout
 
-        val navHostFragment = supportFragmentManager.findFragmentById(R.id.myNavHostFragment) as NavHostFragment
-        val navController = navHostFragment.navController
-        NavigationUI.setupActionBarWithNavController(this, navController, drawerLayout)
-        appBarConfiguration = AppBarConfiguration(navController.graph, drawerLayout)
+        ActivityMainBinding.inflate(layoutInflater).let { binding ->
+            setContentView(binding.root)
+            drawerLayout = binding.drawerLayout
 
-        // prevent nav gesture if not on start destination
-        navController.addOnDestinationChangedListener { nc: NavController, nd: NavDestination, _ ->
-            if (nd.id == nc.graph.startDestinationId) {
-                drawerLayout.setDrawerLockMode(DrawerLayout.LOCK_MODE_UNLOCKED)
-            } else {
-                drawerLayout.setDrawerLockMode(DrawerLayout.LOCK_MODE_LOCKED_CLOSED)
+            val navHostFragment =
+                supportFragmentManager.findFragmentById(R.id.myNavHostFragment) as NavHostFragment
+            navHostFragment.navController.let { navController ->
+                NavigationUI.setupActionBarWithNavController(
+                    this,
+                    navController,
+                    drawerLayout
+                )
+                appBarConfiguration = AppBarConfiguration(navController.graph, drawerLayout)
+
+                // prevent nav gesture if not on start destination
+                navController.addOnDestinationChangedListener { nc: NavController, nd: NavDestination, _ ->
+                    if (nd.id == nc.graph.startDestinationId) {
+                        drawerLayout.setDrawerLockMode(DrawerLayout.LOCK_MODE_UNLOCKED)
+                    } else {
+                        drawerLayout.setDrawerLockMode(DrawerLayout.LOCK_MODE_LOCKED_CLOSED)
+                    }
+                }
+
+                NavigationUI.setupWithNavController(binding.navView, navController)
             }
         }
-
-        NavigationUI.setupWithNavController(binding.navView, navController)
     }
 
     override fun onSupportNavigateUp(): Boolean {
